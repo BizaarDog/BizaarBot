c1c071ffb80ab6a51905c9e8be924ee5ac7ed39b
BizaarDog <36164647+BizaarDog@users.noreply.github.com>
2018-02-05 13:28:22 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const PREFIX = "b"
+const PREFIX = "b!"
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -59,6 +59,24 @@ client.on('guildMemberAdd', member => {
     if (!channel) return;
     channel.send(`Welcome to the server, ${member}`);
 });
-    
+
+ if(command === "b!kick") {
+     if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+         return message.reply("Sorry, you don't have permissions to use this!");
+
+ let member = message.mentions.members.first();    
+ if(!member)
+     return message.reply("Please mention a valid member of this server");
+ if(!member.kickable)
+     return message.reply("I cannot kick this user! That user may have a higher role. I may also not have permission to kick that member");
+ 
+ let reason = args.slice(1).join(' ');
+ if(!reason)
+     return message.reply("Please indicate a reason for the kick!");
+ 
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);    
+     
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
